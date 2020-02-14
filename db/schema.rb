f1a722aff6e9e51f7d9b7b64536ae315bef9b152
Jenny Klich <jennyklich@gmail.com>
2020-02-14 10:34:05 -0700

    Add still fixing relationships

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200214170317) do
+ActiveRecord::Schema.define(version: 20200214173302) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,9 @@ ActiveRecord::Schema.define(version: 20200214170317) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "movie_id"
+    t.index ["movie_id"], name: "index_studios_on_movie_id"
   end
 
+  add_foreign_key "studios", "movies"
 end
--- spec/features/studio/index_spec.rb
+++ spec/features/studio/index_spec.rb
@@ -3,18 +3,25 @@ require 'rails_helper'
 RSpec.describe "Studio Index Page" do
   it "can see a list of all the studios and their movies" do
     studio_1 = Studio.create(name: "Warner Brothers")
+    movie_1 = Movie.create(name: "Harry Potter",
+                          studio: studio_1)
+    movie_2 = Movie.create(name: "Watchmen",
+                          studio: studio_1)
     studio_2 = Studio.create(name: "Universal")
+    movie_3 = Movie.create(name: "Sing",
+                          studio: studio_2)
     studio_3 = Studio.create(name: "Columbia")
-
-    movie_1 = studio_1.movie.create(name: "Harry Potter")
-    movie_2 = studio_1.movie.create(name: "Watchmen")
-    movie_3 = studio_2.movie.create(name: "Sing")
-    movie_4 = studio_3.movie.create(name: "SkyFall")
+    movie_4 = Movie.create(name: "SkyFall",
+                          studio: studio_1)
 
     visit "/studios"
 
-    expect(page).to have_content("Warner Brothers: Harry Potter, Watchmen")
-    expect(page).to have_content("Universal: Sing")
-    expect(page).to have_content("Columbia: SkyFall")
+    expect(page).to have_content("Warner Brothers")
+    expect(page).to have_content("Universal")
+    expect(page).to have_content("Columbia")
+    expect(page).to have_content("Harry Potter")
+    expect(page).to have_content("Watchmen")
+    expect(page).to have_content("Sing")
+    expect(page).to have_content("SkyFall")
   end
 end
